bc8904ae797e94b2dd74860c689afa2a1a7f5c35
greyhands2 <greyhands2@gmail.com>
2022-01-04 15:19:46 +0100

    added clockIn module model

--- services/errorService/errorController.js
+++ services/errorService/errorController.js
@@ -5,8 +5,8 @@ const handleCastErrorDB = err => {
   };
   
   const handleDuplicateFieldsDB = err => {
-	  console.log('the error', err);
-	const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+	  
+	const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
   
 	const message = `Duplicate field value: ${value}. Please use another value!`;
 	
