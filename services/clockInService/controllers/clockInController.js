1bf5803583357175a8f44821408538d5b6823ac2
greyhands2 <greyhands2@gmail.com>
2022-01-04 21:59:01 +0100

    added clockIn module model

--- services/clockInService/controllers/clockInController.js
+++ services/clockInService/controllers/clockInController.js
@@ -12,9 +12,14 @@ exports.addClockIn=catchAsync(async(req,res,next)=>{
 
         if(err) return next(new AppError("Something Went Wrong", 500));
 
-        if(Object.keys(doc).length ===0) return next(new AppError("Staff not found", 404));
+        if(Object.keys(doc).length === 0 || doc.month !== new Date(Date.now()).toLocaleString('default', { month: 'long' })) {
+            await ClockIn.create({staff:req.params.staffId, count:1 });
 
-        if(doc.month === new Date(Date.now()).toLocaleString('default', { month: 'long' })){
+            return res.status(200).json({
+                status:"success",
+                message:"user clock in updated for the new month"
+            })
+        } else {
             doc.count=doc.count+1;
 
             await doc.save();
@@ -23,14 +28,8 @@ exports.addClockIn=catchAsync(async(req,res,next)=>{
                 status:"success",
                 message:"user clock in updated"
             })
-        } else {
-            await ClockIn.create({staff:req.params.staffId, count:1 });
-
-            return res.status(200).json({
-                status:"success",
-                message:"user clock in updated for the new month"
-            })
         }
+        
 
 
 
