31ecf397d60ef3a1e0be75db741b345db1664b2f
greyhands2 <greyhands2@gmail.com>
2023-03-08 14:48:16 +0100

    added aggregation projection stage

--- services/clockInService/controllers/clockInController.js
+++ services/clockInService/controllers/clockInController.js
@@ -85,7 +85,7 @@ exports.getStaffClockin=(type)=>catchAsync(async(req,res,next)=>{
             count: {$first: '$count'},
             staff: {$first: '$staff'},
             email: {$first: '$email'},
-            staffDetails: {'$first': '$staffDetails'}
+            //staffDetails: {'$first': '$staffDetails'}
             
         }
     }
@@ -109,7 +109,7 @@ exports.getStaffClockin=(type)=>catchAsync(async(req,res,next)=>{
         lookup,
         unwind,
         group, 
-        //project
+        project
     ];
     
     
