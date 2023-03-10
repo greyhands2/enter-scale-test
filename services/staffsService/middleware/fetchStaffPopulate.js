a77ba51ec2834ad2723dfeac9f6b867cd0a04e90
greyhands2 <greyhands2@gmail.com>
2023-03-10 13:56:41 +0100

    new stuff

--- services/staffsService/middleware/fetchStaffPopulate.js
+++ services/staffsService/middleware/fetchStaffPopulate.js
@@ -1,6 +1,6 @@
 exports.fetchStaffSetPopulation =  (req, res, next) => {
 	
-	if(req.staff.role === 'staff') req.body.populate= {path: 'clockIns', select: 'count month'};
+	if(req.staff.role === 'staff') req.body.populate= [{path: 'clockIns', select: 'count month'}];
 		
 	
 	
